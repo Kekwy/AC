a68f8cdfb862f03db209f23aebc945ff7fc9cf4b
Kekwy <keekkewy@qq.com>
2023-08-24 19:21:39 +0800

    leetcode/1267

---
+++ cpp/include/leetcode/1267/Solution2.h
@@ -0,0 +1,30 @@
+//
+// Created by Kekwy on 2023/8/13.
+//
+
+#ifndef CPP_SOLUTION_H
+#define CPP_SOLUTION_H
+
+#include <vector>
+
+using namespace std;
+
+/**
+ * 并查集
+ */
+class UFSets {
+
+public:
+    // TODO: union, find, UFSets
+};
+
+
+class Solution {
+
+public:
+    int countServers(vector<vector<int>>& grid) {
+
+    }
+};
+
+#endif //CPP_SOLUTION_H
