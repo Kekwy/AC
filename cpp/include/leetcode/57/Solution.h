4039bd2aabb30bf7e17aa0f5d4a7afb398c02dd7
Kekwy <keekkewy@qq.com>
2023-08-28 13:56:53 +0800

    Meiriyiti

--- cpp/include/leetcode/57/Solution.h
+++ cpp/include/leetcode/57/Solution.h
@@ -14,17 +14,27 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         auto res = vector<vector<int>>();
-        int lBound = intervals[0][0], rBound = intervals[0][1];
-        for (int i = 1; i < intervals.size(); i++) {
-            if (intervals[i][0] > rBound) {
-                res.push_back({lBound, rBound});
-                lBound = intervals[i][0];
-                rBound = intervals[i][1];
-            } else if (intervals[i][1] > rBound) { //
-                rBound = intervals[i][1];
+        bool flag = false;
+        int lBound = newInterval[0], rBound = newInterval[1];
+        for (int i = 0; i < intervals.size(); i++) {
+            if (flag) {
+                res.push_back(intervals[i]);
+            } else {
+                if (rBound < intervals[i][0]) {
+                    res.push_back({lBound, rBound});
+                    res.push_back(intervals[i]);
+                    flag = true;
+                } else if (lBound > intervals[i][1]) {
+                    res.push_back(intervals[i]);
+                } else {
+                    lBound = lBound < intervals[i][0]? lBound : intervals[i][0];
+                    rBound = rBound > intervals[i][1]? rBound : intervals[i][1];
+                }
             }
         }
-        res.push_back({lBound, rBound});
+        if (!flag) {
+            res.push_back({lBound, rBound});
+        }
         return res;
     }
 };
