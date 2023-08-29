2f42f4e89b465d353cc1bc63e4e34efa25235dd8
Kekwy <keekkewy@qq.com>
2023-08-29 15:37:42 +0800

    leetcode/54

--- cpp/include/leetcode/54/Solution.h
+++ cpp/include/leetcode/54/Solution.h
@@ -14,6 +14,7 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         auto res = vector<int>();
+        if (matrix.empty()) return res;
         auto visited = vector<vector<bool>>();
         for (int i = 0; i < matrix.size(); i++) {
             visited.push_back(vector<bool>());
@@ -21,7 +22,10 @@ public:
                 visited[i].push_back(false);
             }
         }
-        int i = 0, j = -1;
+        // int i = 0, j = -1; // 写法错误，第一次纵向遍历
+        res.push_back(matrix[0][0]);
+        visited[0][0] = true;
+        int i = 0, j = 0;
         while (true) {
             bool flag = true;
             for(; j + 1 < visited[i].size() && !visited[i][j + 1]; j++) {
