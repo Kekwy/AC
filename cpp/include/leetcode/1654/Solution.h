509fc79dc82e5262402337e59b128429a60cd61a
Kekwy <keekkewy@qq.com>
2023-08-30 12:26:10 +0800

    leetcode/1654 深搜DP（超时且有死循环）

--- cpp/include/leetcode/1654/Solution.h
+++ cpp/include/leetcode/1654/Solution.h
@@ -56,52 +56,23 @@ class Solution {
 public:
     int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
         if (x == 0) return 0;
-        Edge *graph[ARR_SIZE] = {nullptr};
         bool isForbidden[ARR_SIZE] = {false};
-        bool visited[ARR_SIZE] = {false};
         for (int i = 0; i < forbidden.size(); i++) {
             isForbidden[forbidden[i]] = true;
         }
 
-        // 利用广度优先建图
-        auto buildBFS = queue<int>();
-        buildBFS.push(0);
-        while (!buildBFS.empty()) {
-            int i = buildBFS.front();
-            buildBFS.pop();
-            int r = i + a;
-            int l = i - b;
-            if (r < ARR_SIZE && r - b * 2 <= x && !isForbidden[r]) {
-                graph[i] = new Edge(r, false, graph[i]);
-                if (r != x && !visited[r]) {
-                    buildBFS.push(r);
-                }
-            }
-            if (l >= 0 && !isForbidden[l]) {                // else if
-                graph[i] = new Edge(l, true, graph[i]);     // new Edge(r, true, graph[i]);
-                if (l != x && !visited[l]) {
-                    buildBFS.push(l);
-                }
-            }
-            visited[i] = true; // 避免死循环
-        }
+        // 利用广度优先搜索找到到达 x 的最短路径
+        auto searchBFS = queue<int[3]>();
+        searchBFS.push({0, 0, 0}); // {结点编号，由回退几次到达，广搜的第几层到达}
         
-        // 利用动态规划进行优化，dp[i][j] 表示由回退 j 次到达 i 结点时，i 结点到 x 结点的最短路径
-        int dp[ARR_SIZE][3] = {{0, 0, 0}};
+        // 利用动态规划进行优化，dp[i][j] 表示由连续回退 j 次到达 i 结点时，i 结点到 x 结点的最短路径
+        bool visited[ARR_SIZE][3] = {{false, false, false}};
 
-        runDP(0, x, 0, graph, dp);
-
-        int res = dp[0][0];
-
-        // 归还内存
-        for (int i = 0; i < ARR_SIZE; i++) {
-            Edge *p = graph[i];
-            Edge *q = nullptr;
-            while (p != nullptr) {
-                q = p;
-                p = p->next;
-                delete q;
-            }
+        int res = -1;
+        while (!searchBFS.empty()) {
+            int i = searchBFS.front()[0];
+            int back = searchBFS.front()[1];
+            int length = searchBFS.front()[2];
         }
 
         return res;
