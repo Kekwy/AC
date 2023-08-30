401aaae2ee90a0a2f180dfcfa3b5346e7902cb11
Kekwy <keekkewy@qq.com>
2023-08-30 15:23:36 +0800

    leetcode/1654

--- cpp/include/leetcode/1654/Solution.h
+++ cpp/include/leetcode/1654/Solution.h
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-#define ARR_SIZE 4000
+#define ARR_SIZE 40000
 
 /**
  * tips: Think of the line as a graph.
@@ -30,7 +30,11 @@ public:
         searchBFS.push({0, 0, 0}); // {结点编号，由回退几次到达，广搜的第几层到达}
         
         // 利用动态规划进行优化，dp[i][j] 表示由连续回退 j 次到达 i 结点时，i 结点到 x 结点的最短路径
-        bool visited[ARR_SIZE][3] = {{false, false, false}};
+        // bool visited[ARR_SIZE][3] = {{false, false, false}}; // 审题有误，实际上写成了不能连续回跳三次的情况
+        bool visited[ARR_SIZE][2] = {{false, false}};
+
+        visited[0][0]  = true;
+        visited[0][1] = true;
 
         int res = -1;
         while (!searchBFS.empty()) {
@@ -39,7 +43,7 @@ public:
             int length = searchBFS.front()[2];
             searchBFS.pop();
 
-            visited[i][back] = true; // 避免死循环
+            if (back && visited[i][0]) continue;
 
             if (i == x) { // 由于使用广搜，后续找到的路径只可能比当前值大
                 res = length;
@@ -47,14 +51,18 @@ public:
             } else {
                 int r = i + a;
                 int l = i - b;
-                if (r < ARR_SIZE && !isForbidden[r] && (r <= x || a < b * 2)) {
+                if (r < ARR_SIZE && !isForbidden[r]) { 
+                    // 算法停止运行的条件不是很合理，脱离了问题规模，且在当前情境下该常数总是大于问题规模
                     if (!visited[r][0]) {
                         searchBFS.push({r, 0, length + 1});
+                        visited[r][0] = true; // 避免死循环
+                        visited[r][1] = true;
                     }
                 }
-                if (l >= 0 && !isForbidden[l]) {
-                    if (back < 2 && !visited[l][back + 1]) {
-                        searchBFS.push({l, back + 1, length + 1});
+                if (l >= 0 && !isForbidden[l] && !back) {
+                    if (!visited[l][1]) {
+                        searchBFS.push({l, 1, length + 1});
+                        visited[l][1] = true;
                     }
                 }
             }
