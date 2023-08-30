700bdd29b09237ed4ae31cd0e5c21e1219b6b22f
Kekwy <keekkewy@qq.com>
2023-08-30 15:14:38 +0800

    leetcode/1654

--- cpp/include/leetcode/1654/Solution.h
+++ cpp/include/leetcode/1654/Solution.h
@@ -17,42 +17,6 @@ using namespace std;
  */
 class Solution {
 
-    struct Edge {
-        int to;
-        bool isBack;
-        Edge *next;
-        Edge(int to, bool isBack, Edge *next): to(to), isBack(isBack), next(next) {}
-    };
-
-    void runDP(int i, int dst, int back, Edge *graph[], int dp[][3]) {
-        Edge *p = graph[i];
-        dp[i][back] = -1;
-        while (p != nullptr) {
-            int tmpBack = 0;
-            if (p->isBack) {
-                tmpBack = back + 1;
-            }
-            if (tmpBack < 2) {
-                if (dp[p->to][tmpBack] == -1) {
-                    continue;
-                } else {
-                    if (p->to == dst) {
-                        dp[i][back] = 1;
-                        return;
-                    } else {
-                        if (dp[p->to][tmpBack] == 0) {
-                            runDP(p->to, dst, tmpBack, graph, dp);
-                        }
-                        if (dp[i][back] == -1 || dp[i][back] > dp[p->to][tmpBack] + 1) {
-                            dp[i][back] = dp[p->to][tmpBack] + 1;
-                        }
-                    }
-                }
-            }
-            p = p->next;
-        }
-    }
-
 public:
     int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
         if (x == 0) return 0;
@@ -62,7 +26,7 @@ public:
         }
 
         // 利用广度优先搜索找到到达 x 的最短路径
-        auto searchBFS = queue<int[3]>();
+        auto searchBFS = queue<vector<int>>();
         searchBFS.push({0, 0, 0}); // {结点编号，由回退几次到达，广搜的第几层到达}
         
         // 利用动态规划进行优化，dp[i][j] 表示由连续回退 j 次到达 i 结点时，i 结点到 x 结点的最短路径
@@ -73,8 +37,28 @@ public:
             int i = searchBFS.front()[0];
             int back = searchBFS.front()[1];
             int length = searchBFS.front()[2];
-        }
+            searchBFS.pop();
 
+            visited[i][back] = true; // 避免死循环
+
+            if (i == x) { // 由于使用广搜，后续找到的路径只可能比当前值大
+                res = length;
+                break;
+            } else {
+                int r = i + a;
+                int l = i - b;
+                if (r < ARR_SIZE && !isForbidden[r] && (r <= x || a < b * 2)) {
+                    if (!visited[r][0]) {
+                        searchBFS.push({r, 0, length + 1});
+                    }
+                }
+                if (l >= 0 && !isForbidden[l]) {
+                    if (back < 2 && !visited[l][back + 1]) {
+                        searchBFS.push({l, back + 1, length + 1});
+                    }
+                }
+            }
+        }
         return res;
     }
 };
