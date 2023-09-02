03728968117c31f9a36a56eeb9ba1bb9a9c50e0c
Kekwy <keekkewy@qq.com>
2023-09-02 23:46:20 +0800

    leetcode/offer-64 怪题，短路求值的活用

--- cpp/include/leetcode/offer-64/Solution.h
+++ cpp/include/leetcode/offer-64/Solution.h
@@ -5,39 +5,15 @@
 #ifndef CPP_SOLUTION_H
 #define CPP_SOLUTION_H
 
-#include <queue>
-#include <vector>
-
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- */
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- };
-
 class Solution {
-    
-    int balancedHeight(TreeNode* root) {
-        if (root == nullptr) return 0;
-        int leftHeight = balancedHeight(root->left);
-        if (leftHeight == -1) return -1;
-        int rightHeight = balancedHeight(root->right);
-        if (rightHeight == -1) return -1;
-        if (abs(leftHeight - rightHeight) > 1) return -1;
-        return 1 + max(leftHeight, rightHeight);
-    }
-
 public:
-
-    bool isBalanced(TreeNode* root) {
-        return balancedHeight(root) != -1;
+    int sumNums(int n) {
+        n && (n += sumNums(n-1));
+        return n;
     }
-    
 };
+
 
 #endif //CPP_SOLUTION_H
