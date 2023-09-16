953371f42d30dff214a9a908c20e405d1f71b770
Kekwy <keekkewy@qq.com>
2023-09-16 13:54:30 +0800

    leetcode/31 优化

--- cpp/include/leetcode/31/Solution.h
+++ cpp/include/leetcode/31/Solution.h
@@ -16,12 +16,10 @@ public:
     void nextPermutation(vector<int> &nums) {
         int i, j;
         for (i = nums.size() - 2; i >= 0 && nums[i] >= nums[i + 1]; i--);
-        if (i < 0) {
-            sort(nums.begin(), nums.end());
-            return;
+        if (i >= 0) {
+            for (j = nums.size() - 1; j > 0 && nums[j] <= nums[i]; j--);
+            swap(nums[i], nums[j]);
         }
-        for (j = nums.size() - 1; j > 0 && nums[j] <= nums[i]; j--);
-        swap(nums[i], nums[j]);
         reverse(nums.begin() + i + 1, nums.end());
     }
 };
