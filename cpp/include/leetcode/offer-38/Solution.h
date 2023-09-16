b97e5c406749ad8603f6db097d956e739bb94971
Kekwy <keekkewy@qq.com>
2023-09-16 14:01:49 +0800

    leetcode/offer-38

--- cpp/include/leetcode/offer-38/Solution.h
+++ cpp/include/leetcode/offer-38/Solution.h
@@ -12,10 +12,28 @@
 using namespace std;
 
 class Solution {
+
+    bool nextPermutation(string &str) {
+        int i, j;
+        for (i = str.length() - 2; i >= 0 && str[i] >= str[i + 1]; i--);
+        if (i >= 0) {
+            for (j = str.length() - 1; j > 0 && str[j] <= str[i]; j--);
+            swap(str[i], str[j]);
+            reverse(str.begin() + i + 1, str.end());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 public:
     vector<string> permutation(string s) {
         sort(s.begin(), s.end());
-        
+        vector<string> res;
+        do {
+            res.push_back(s);
+        } while (nextPermutation(s));
+        return res;
     }
 };
 
