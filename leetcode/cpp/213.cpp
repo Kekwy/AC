00bcdd09462218d03d6ed5ccbfcdd31537a8f966
Kekwy <keekkewy@qq.com>
2023-09-18 01:03:28 +0800

    leetcode/213

--- leetcode/cpp/213.cpp
+++ leetcode/cpp/213.cpp
@@ -1,3 +1,10 @@
+// @lcpr-before-debug-begin
+
+
+//cvcxzv
+
+// @lcpr-before-debug-end
+
 /*
  * @lc app=leetcode.cn id=213 lang=cpp
  * @lcpr version=21913
@@ -22,14 +29,26 @@ using namespace std;
 #include <vector>
 // @lc code=start
 class Solution {
-public:
-    int rob(vector<int>& nums) {
 
+    int rangeRob(vector<int> &nums, int begin, int end) {
+        if (begin >= end) return 0;
+        int size = end - begin + 1;
+        auto dp = vector<int>(size);
+        dp[size - 1] = 0;
+        dp[size - 2] = nums[end - 1];
+        for (int i = end - 2; i >= begin; i--) {
+            dp[i - begin] = max(nums[i] + dp[i + 2 - begin], dp[i + 1 - begin]);
+        }
+        return dp[0];
+    }
+
+public:
+    int rob(vector<int> &nums) {
+        if (nums.size() == 1) return nums[0];
+        return max(rangeRob(nums, 0, nums.size() - 1), rangeRob(nums, 1, nums.size()));
     }
 };
 // @lc code=end
-
-
 
 /*
 // @lcpr case=start
